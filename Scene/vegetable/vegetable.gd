450d1db4fe7b42226234c8d2db53b77ef92be862
heulll <hoel.rubin@gmail.com>
2024-02-13 20:25:39 +0100

    vegetable basics

---
+++ Scene/vegetable/vegetable.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name  Vegetable
+
+@export var vegetable_name : String
+
+@export var price: int 
+
+@export var stat_catastrophe : Array = [0,0,0,0] #0 = tramblement de terre; 1 = incendi; 2 = tornade ; 3 = tsunami
+@export var effect_description : String
+
+@export var growing_time : int
+var current_growState : int
+
+var grid_position : Vector2
+
