5cec286856082cf20a1117f0855563ef1cb76dbf
heulll <hoel.rubin@gmail.com>
2024-02-14 11:39:10 +0100

    icon and stat barre

--- Scene/vegetable/vegetable.gd
+++ Scene/vegetable/vegetable.gd
@@ -2,11 +2,12 @@ extends TextureRect
 class_name  Vegetable
 
 @export var vegetable_name : String
+@export var type : String
 
 @export var price: int 
 
 @export var stat_catastrophe : Array = [0,0,0,0] #0 = tramblement de terre; 1 = incendi; 2 = tornade ; 3 = tsunami
-@export var effect_description : String
+@export var effect_description : String = "je donne 1 point de ..."
 
 @export var growing_time : int
 var current_growState : int
--- Scene/vegetable/vegetableScript/turnip.gd
+++ Scene/vegetable/vegetableScript/turnip.gd
@@ -1,1 +1,14 @@
 extends Vegetable
+
+
+# pas les bonnes stats
+
+func effect(adjacent : Array):
+	
+	for n in adjacent:
+		if adjacent[n] == null:
+			stat_catastrophe[3] += 2
+			pass
+		pass
+	
+	pass
